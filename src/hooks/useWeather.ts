e1ff5db342f3e173d915f9906e8137f4ed87798e
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-03 15:03:34 -0300

    Segundo llamado hacia la API para obtener el Clima

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -12,6 +12,13 @@ export default function useWeather() {
       const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&appid=${appID}`
       
       const { data } = await axios(geoURL)
+      const lat = data[0].lat
+      const lon = data[0].lon
+      const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appID}`
+      
+      const { data: weatherData } = await axios(weatherURL)
+      console.log(weatherData.main);
+      
 
     } catch (error) {
       console.error(error);
